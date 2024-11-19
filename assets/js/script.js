fd37b8f17571988f90c2bd1579b1ae34e0abcef4
CatMamaGB <165972971+CatMamaGB@users.noreply.github.com>
2024-11-18 23:31:21 -0600

    createtaskcard function

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,10 +19,17 @@ function createTaskCard(task) {
         </div>
     `).draggable({
         revert: 'invalid',
+    });
+    return taskCard;
 }
 
 // Todo: create a function to render the task list and make cards draggable
 function renderTaskList() {
+    $('.task-card').draggable({
+        revert: 'invalid',
+        stack: '.task-card'
+    });
+
 
 }
 
@@ -37,11 +44,11 @@ function handleDeleteTask(event){
 }
 
 // Todo: create a function to handle dropping a task into a new status lane
-function handleDrop(event, ui) {
+function handleDrop(event, ui) {                                
 
 }
 
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
 $(document).ready(function () {
-
+    renderTaskList();
 });
